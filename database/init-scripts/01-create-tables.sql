8021d1dde19fbdae3c5014c8301dd96b5e9196c2
rosudavidg <rosudavidg@gmail.com>
2020-12-11 22:25:17 +0200

    add base for database

---
+++ database/init-scripts/01-create-tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    temporary BOOLEAN NOT NULL,
+    email VARCHAR (64),
+    activated BOOLEAN DEFAULT FALSE NOT NULL,
+    nickname VARCHAR (64) NOT NULL,
+    created_date TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS heuristics (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (32)
+);
+
+CREATE TABLE IF NOT EXISTS quizzes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) NOT NULL,
+    short_url VARCHAR (64) NOT NULL,
+    question VARCHAR (256) NOT NULL,
+    heuristic_id INTEGER REFERENCES heuristics(id) NOT NULL,
+    answers_target INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS t_options (
+    id SERIAL PRIMARY KEY,
+    quizz_id INTEGER REFERENCES quizzes(id) NOT NULL ON DELETE CASCADE,
+    text VARCHAR (128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS answers (
+    id SERIAL PRIMARY KEY,
+    quiz_id INTEGER REFERENCES quizzes(id) NOT NULL ON DELETE CASCADE,
+    option_id INTEGER REFERENCES t_options(id) NOT NULL ON DELETE CASCADE,
+    index_order INTEGER NOT NULL
+);

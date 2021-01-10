ec032540e80e71dfe80c43802cc15539f7bae59b
rosudavidg <rosudavidg@gmail.com>
2021-01-10 15:16:09 +0200

    add POST /quizzes

--- database/init-scripts/02-insert-heuristics.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO heuristics (name)
-VALUES ('FIRST_ADDED');
-
-
-INSERT INTO heuristics (name)
-VALUES ('ALPHABETICAL');
-
--- io/database.py
+++ io/database.py
@@ -37,11 +37,11 @@ def insert_user(connection, email, password, nickname):
     return user_id
 
 
-def insert_quiz(connection, user_id, short_url, question, heuristic_id, answers_target):
+def insert_quiz(connection, user_id, short_url, question, answers_target):
     cursor = connection.cursor()
 
-    sql = "INSERT INTO quizzes (user_id, short_url, question, heuristic_id, answers_target) VALUES (%s, %s, %s, %s, %s) RETURNING id"
-    val = (user_id, short_url, question, heuristic_id, answers_target)
+    sql = "INSERT INTO quizzes (user_id, short_url, question, answers_target) VALUES (%s, %s, %s, %s) RETURNING id"
+    val = (user_id, short_url, question, answers_target)
 
     try:
         cursor.execute(sql, val)
@@ -223,6 +223,59 @@ def get_quiz_id(connection, short_url):
         connection.commit()
 
         return cursor.fetchone()[0]
+
+    except:
+        connection.rollback()
+        raise Exception()
+
+    cursor.close()
+
+
+def answer(connection, quiz_id, user_id, option_id):
+    cursor = connection.cursor()
+
+    sql = "INSERT INTO answers (user_id, quiz_id, option_id) VALUES (%s, %s, %s)"
+    val = (quiz_id, user_id, option_id)
+
+    try:
+        cursor.execute(sql, val)
+        connection.commit()
+
+    except:
+        connection.rollback()
+        raise Exception()
+
+    cursor.close()
+
+
+def result(connection, quiz_id):
+    cursor = connection.cursor()
+
+    sql = "SELECT COUNT(*) FROM answers WHERE quiz_id = %s"
+    val = (quiz_id,)
+
+    sql_req = "SELECT answers_target FROM quizzes WHERE id = %s"
+    val_req = (quiz_id,)
+
+    sql_res = "SELECT count(*) as cnt, option_id FROM answers WHERE quiz_id = %s GROUP BY option_id ORDER BY cnt DESC"
+    val_res = (quiz_id,)
+
+    sql_add_res = "INSERT INTO results (quiz_id, option_id) VALUES (%s, %s)"
+
+    try:
+        cursor.execute(sql, val)
+        answer_count = cursor.fetchone()[0]
+
+        cursor.execute(sql_req, val_req)
+        answers_target = cursor.fetchone()[0]
+
+        if answer_count == answers_target:
+            cursor.execute(sql_res, val_res)
+            result_id = cursor.fetchall()[0][1]
+
+            val_add_res = (quiz_id, result_id)
+            cursor.execute(sql_add_res, val_add_res)
+            connection.commit()
 
     except:
         connection.rollback()
